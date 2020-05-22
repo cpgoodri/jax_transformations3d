c5c83a1e508ec7b6a9ff8a3d597622b59a2e215d
Goodrich, Carl <carlpgoodrich@gmail.com>
2020-05-22 16:28:30 -0700

    Add more functions

--- tests.py
+++ tests.py
@@ -1,36 +1,165 @@
 from transformations import *
 
+from absl.testing import absltest
+from absl.testing import parameterized
 
-def test_identity_matrix():
-  I = identity_matrix()
-  assert(np.allclose(I, np.dot(I, I)))
-  assert(np.allclose(np.array([np.sum(I), np.trace(I)]), np.array([4.0, 4.0])))
-  assert(np.allclose(I, np.identity(4)))
-  print('identity_matrix() passed')
+from jax import test_util as jtu
 
-
-def test_translation_matrix():
-  v = np.array(onp.random.random(3)) - 0.5
-  assert(np.allclose(v, translation_matrix(v)[:3, 3]))
-  print('translation_matrix() passed')
-
-
-def test_translation_from_matrix():
-  v0 = np.array(onp.random.random(3)) - 0.5
-  v1 = translation_from_matrix(translation_matrix(v0))
-  assert(np.allclose(v0, v1))
-  print('translation_from_matrix() passed')
+from jax.config import config as jax_config
+jax_config.parse_flags_with_absl()
+FLAGS = jax_config.FLAGS
 
 
 
 
+class TransformationsTest(jtu.JaxTestCase):
+  def test_identity_matrix(self):
+    I = identity_matrix()
+    self.assertAllClose(I, np.dot(I, I), True)
+    self.assertAllClose(np.array([np.sum(I), np.trace(I)]), np.array([4.0, 4.0]), True)
+    self.assertAllClose(I, np.identity(4), True)
+
+
+  def test_translation_matrix(self):
+    v = np.array(onp.random.random(3)) - 0.5
+    self.assertAllClose(v, translation_matrix(v)[:3, 3], True)
+
+
+  def test_translation_from_matrix(self):
+    v0 = np.array(onp.random.random(3)) - 0.5
+    v1 = translation_from_matrix(translation_matrix(v0))
+    self.assertAllClose(v0, v1, True)
+    
+
+  def test_reflection_matrix(self):
+    v0 = onp.random.random(4) - 0.5
+    v0[3] = 1.
+    v0 = np.array(v0)
+    v1 = np.array(onp.random.random(3)) - 0.5
+    R = reflection_matrix(v0, v1)
+    self.assertAllClose(2, np.trace(R), False)
+    self.assertAllClose(v0, np.dot(R, v0), True)
+    v2 = onp.array(v0).copy()
+    v3 = onp.array(v0).copy()
+    v2[:3] += v1
+    v3[:3] -= v1
+    v2 = np.array(v2)
+    v3 = np.array(v3)
+    self.assertAllClose(v2, np.dot(R, v3), True)
+
+
+  def test_reflection_from_matrix(self):
+    v0 = np.array(onp.random.random(3)) - 0.5
+    v1 = np.array(onp.random.random(3)) - 0.5
+    M0 = reflection_matrix(v0, v1)
+    point, normal = reflection_from_matrix(M0)
+    M1 = reflection_matrix(point, normal)
+    #is_same_transform(M0, M1)
+    M0 /= M0[3,3]
+    M1 /= M1[3,3]
+    self.assertAllClose(M0,M1,True)
+    
+  def test_rotation_matrix(self):
+    R = rotation_matrix(math.pi/2, np.array([0, 0, 1]), np.array([1, 0, 0]))
+    self.assertAllClose(np.dot(R, np.array([0, 0, 0, 1])), np.array([1, -1, 0, 1]), False)
+    
+    """
+    >>> angle = (random.random() - 0.5) * (2*math.pi)
+    >>> direc = numpy.random.random(3) - 0.5
+    >>> point = numpy.random.random(3) - 0.5
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(angle-2*math.pi, direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> R0 = rotation_matrix(angle, direc, point)
+    >>> R1 = rotation_matrix(-angle, -direc, point)
+    >>> is_same_transform(R0, R1)
+    True
+    >>> I = numpy.identity(4, numpy.float64)
+    >>> numpy.allclose(I, rotation_matrix(math.pi*2, direc))
+    True
+    >>> numpy.allclose(2, numpy.trace(rotation_matrix(math.pi/2,
+    ...                                               direc, point)))
+    True
+    """
 
 
 
 
 
-test_identity_matrix()
-test_translation_matrix()
-test_translation_from_matrix()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def test_unit_vector(self):
+    v0 = np.array(onp.random.random(3))
+    v1 = unit_vector(v0)
+    self.assertAllClose(v1, v0 / np.linalg.norm(v0), True)
+    #>>> v0 = numpy.random.rand(5, 4, 3)
+    #>>> v1 = unit_vector(v0, axis=-1)
+    #>>> v2 = v0 / numpy.expand_dims(numpy.sqrt(numpy.sum(v0*v0, axis=2)), 2)
+    #>>> numpy.allclose(v1, v2)
+    #True
+    #>>> v1 = unit_vector(v0, axis=1)
+    #>>> v2 = v0 / numpy.expand_dims(numpy.sqrt(numpy.sum(v0*v0, axis=1)), 1)
+    #>>> numpy.allclose(v1, v2)
+    #True
+    #>>> v1 = numpy.empty((5, 4, 3))
+    #>>> unit_vector(v0, axis=1, out=v1)
+    ##>>> numpy.allclose(v1, v2)
+    #True
+    #>>> list(unit_vector([]))
+    #[]
+    self.assertAllClose(unit_vector([1]), np.array([1.0]), True)
+    #[1.0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  absltest.main()
 
--- transformations.py
+++ transformations.py
@@ -30,21 +30,219 @@ def translation_matrix(direction):
   """
   M = np.identity(4)
   M = index_update(M, index[:3,3], direction[:3])
-  #M[:3, 3] = direction[:3]
   return M
 
 
 def translation_from_matrix(matrix):
-    """Return translation vector from translation matrix.
+  """Return translation vector from translation matrix.
 
-    >>> v0 = numpy.random.random(3) - 0.5
-    >>> v1 = translation_from_matrix(translation_matrix(v0))
-    >>> numpy.allclose(v0, v1)
-    True
+  >>> v0 = numpy.random.random(3) - 0.5
+  >>> v1 = translation_from_matrix(translation_matrix(v0))
+  >>> numpy.allclose(v0, v1)
+  True
 
-    """
-    return np.array(matrix, copy=False)[:3, 3].copy()
+  """
+  return np.array(matrix, copy=False)[:3, 3].copy()
 
+
+def reflection_matrix(point, normal):
+  """Return matrix to mirror at plane defined by point and normal vector.
+
+  >>> v0 = numpy.random.random(4) - 0.5
+  >>> v0[3] = 1.
+  >>> v1 = numpy.random.random(3) - 0.5
+  >>> R = reflection_matrix(v0, v1)
+  >>> numpy.allclose(2, numpy.trace(R))
+  True
+  >>> numpy.allclose(v0, numpy.dot(R, v0))
+  True
+  >>> v2 = v0.copy()
+  >>> v2[:3] += v1
+  >>> v3 = v0.copy()
+  >>> v2[:3] -= v1
+  >>> numpy.allclose(v2, numpy.dot(R, v3))
+  True
+
+  """
+  normal = unit_vector(normal[:3])
+  M = np.identity(4)
+  M = index_add(M, index[:3,:3], -2.0*np.outer(normal,normal))
+  M = index_update(M, index[:3,3], (2.0 * np.dot(point[:3], normal)) * normal)
+  #M[:3, :3] -= 2.0 * numpy.outer(normal, normal)
+  #M[:3, 3] = (2.0 * numpy.dot(point[:3], normal)) * normal
+  return M
+
+
+def reflection_from_matrix(matrix):
+  """Return mirror plane point and normal vector from reflection matrix.
+
+  >>> v0 = numpy.random.random(3) - 0.5
+  >>> v1 = numpy.random.random(3) - 0.5
+  >>> M0 = reflection_matrix(v0, v1)
+  >>> point, normal = reflection_from_matrix(M0)
+  >>> M1 = reflection_matrix(point, normal)
+  >>> is_same_transform(M0, M1)
+  True
+
+  """
+  M = np.array(matrix, dtype=np.float64, copy=False)
+  # normal: unit eigenvector corresponding to eigenvalue -1
+  w, V = np.linalg.eig(M[:3, :3])
+  i = np.where(abs(np.real(w) + 1.0) < 1e-8)[0]
+  if not len(i):
+    raise ValueError('no unit eigenvector corresponding to eigenvalue -1')
+  normal = np.real(V[:, i[0]]).squeeze()
+  # point: any unit eigenvector corresponding to eigenvalue 1
+  w, V = np.linalg.eig(M)
+  i = np.where(abs(np.real(w) - 1.0) < 1e-8)[0]
+  if not len(i):
+    raise ValueError('no unit eigenvector corresponding to eigenvalue 1')
+  point = np.real(V[:, i[-1]]).squeeze()
+  point /= point[3]
+  return point, normal
+
+
+def rotation_matrix(angle, direction, point=None):
+  """Return matrix to rotate about axis defined by point and direction.
+
+  >>> R = rotation_matrix(math.pi/2, [0, 0, 1], [1, 0, 0])
+  >>> numpy.allclose(numpy.dot(R, [0, 0, 0, 1]), [1, -1, 0, 1])
+  True
+  >>> angle = (random.random() - 0.5) * (2*math.pi)
+  >>> direc = numpy.random.random(3) - 0.5
+  >>> point = numpy.random.random(3) - 0.5
+  >>> R0 = rotation_matrix(angle, direc, point)
+  >>> R1 = rotation_matrix(angle-2*math.pi, direc, point)
+  >>> is_same_transform(R0, R1)
+  True
+  >>> R0 = rotation_matrix(angle, direc, point)
+  >>> R1 = rotation_matrix(-angle, -direc, point)
+  >>> is_same_transform(R0, R1)
+  True
+  >>> I = numpy.identity(4, numpy.float64)
+  >>> numpy.allclose(I, rotation_matrix(math.pi*2, direc))
+  True
+  >>> numpy.allclose(2, numpy.trace(rotation_matrix(math.pi/2,
+  ...                                               direc, point)))
+  True
+
+  """
+  sina = math.sin(angle)
+  cosa = math.cos(angle)
+  direction = unit_vector(direction[:3])
+  # rotation matrix around unit vector
+  R = np.diag(np.array([cosa, cosa, cosa]))
+  R = R + np.outer(direction, direction) * (1.0 - cosa)
+  direction = direction * sina
+  R = R + np.array([[ 0.0,         -direction[2],  direction[1]],
+                    [ direction[2], 0.0,          -direction[0]],
+                    [-direction[1], direction[0],  0.0]])
+  M = np.identity(4)
+  M = index_update(M, index[:3, :3], R)
+  if point is not None:
+    # rotation not around origin
+    point = np.array(point[:3], dtype=np.float64, copy=False)
+    M = index_update(M, index[:3, 3], point - np.dot(R, point))
+  return M
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def unit_vector(data, axis=None, out=None):
+  """Return ndarray normalized by length, i.e. Euclidean norm, along axis.
+
+  COMMENT(@cpgoodri): For now, I am only implementing this for axis=None and out=None. I don't *think* this will affect internal functionality.
+
+  >>> v0 = numpy.random.random(3)
+  >>> v1 = unit_vector(v0)
+  >>> numpy.allclose(v1, v0 / numpy.linalg.norm(v0))
+  True
+  >>> v0 = numpy.random.rand(5, 4, 3)
+  >>> v1 = unit_vector(v0, axis=-1)
+  >>> v2 = v0 / numpy.expand_dims(numpy.sqrt(numpy.sum(v0*v0, axis=2)), 2)
+  >>> numpy.allclose(v1, v2)
+  True
+  >>> v1 = unit_vector(v0, axis=1)
+  >>> v2 = v0 / numpy.expand_dims(numpy.sqrt(numpy.sum(v0*v0, axis=1)), 1)
+  >>> numpy.allclose(v1, v2)
+  True
+  >>> v1 = numpy.empty((5, 4, 3))
+  >>> unit_vector(v0, axis=1, out=v1)
+  >>> numpy.allclose(v1, v2)
+  True
+  >>> list(unit_vector([]))
+  []
+  >>> list(unit_vector([1]))
+  [1.0]
+
+  """
+  if out is None:
+    data = np.array(data, dtype=np.float64, copy=True)
+    if data.ndim == 1:
+      data /= math.sqrt(np.dot(data, data))
+      return data
+  else:
+    assert(False)
+    #if out is not data:
+    #  out[:] = numpy.array(data, copy=False)
+    #data = out
+  length = np.atleast_1d(np.sum(data*data, axis))
+  #np.sqrt(length, length)
+  length = np.sqrt(length)
+  if axis is not None:
+    assert(False)
+    #length = numpy.expand_dims(length, axis)
+  data /= length
+  if out is None:
+    return data
 
 
 
